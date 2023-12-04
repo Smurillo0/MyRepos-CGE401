49335d92c852ac862dc143304b0cb1cce66046a1
Smurillo0 <144478992+Smurillo0@users.noreply.github.com>
2023-12-04 14:43:46 -0600

    prototype 5

---
+++ Protoype5Walkthrough/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(true)
+        {
+            //wait 1 second
+            yield return new WaitForSeconds(spawnRate);
+
+            //pick a random index between 0 and the # of prefabs
+            int index = Random.Range(0, targets.Count);
+
+            //spawn prefab at rabdom selected index
+            Instantiate(targets[index]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Protoype5Walkthrough/Assets/Scripts/Target.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        //add a force upwards multupied by a randomized speed 
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+
+        //add a torque (rotational force)0 with a randomized 
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        //set the position w a randomized X value
+
+        transform.position = RandomSpawnPos();
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+       return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
