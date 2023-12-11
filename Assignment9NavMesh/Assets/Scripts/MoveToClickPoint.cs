8bd31f1e0b6614d31c4c6888f655cc9a39613037
Smurillo0 <144478992+Smurillo0@users.noreply.github.com>
2023-12-11 02:15:41 -0600

    finished01 added

---
+++ Assignment9NavMesh/Assets/Scripts/MoveToClickPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToClickPoint : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            //vector of position where mouse was clicked
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            //if hit, put in new destination
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+            
+        }
+    }
+}
